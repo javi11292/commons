4f4d2df860bea5516ebdc4419af7e8b00e7a6315
Javier Robles <javiroblesfernandez@gmail.com>
2022-11-01 02:13:39 +0100

    Add snackbar

--- components/snackbar/index.tsx
+++ components/snackbar/index.tsx
@@ -1,20 +1,65 @@
-import { observable } from "mobx";
+import { action, observable } from "mobx";
 import { observer } from "mobx-react-lite";
+import { useEffect, useRef, useState } from "react";
 
-type Message = { value: string };
+import Transition from "commons/components/transition";
+import { classNames } from "commons/utils";
 
+type Message = { text: string; type?: "info" | "error" };
+
+const DELAY = 3000;
 const messages = observable<Message>([]);
 
-export function addNotification(message: Message) {
+const removeMessage = action(function removeNotification() {
+  messages.shift();
+});
+
+export const addMessage = action(function addNotification(message: Message) {
   messages.push(message);
-}
+});
 
 function Snackbar() {
-  const [message] = messages;
+  const [firstMessage] = messages;
+  const [show, setShow] = useState(false);
+  const message = useRef<Message>();
 
-  if (!message) return null;
+  function handleTransitionEnd(
+    { propertyName }: React.TransitionEvent<HTMLDivElement>,
+    transition: boolean
+  ) {
+    if (propertyName !== "opacity") {
+      return;
+    }
 
-  return <div>{message.value}</div>;
+    if (transition) {
+      setTimeout(() => {
+        setShow(false);
+      }, DELAY);
+    } else {
+      removeMessage();
+    }
+  }
+
+  useEffect(() => {
+    message.current = firstMessage;
+    setShow(!!firstMessage);
+  }, [firstMessage]);
+
+  return (
+    <Transition
+      show={show}
+      className={classNames(
+        "fixed bottom-0 left-1/2 -translate-x-1/2 translate-y-full rounded p-3 opacity-0",
+        message.current?.type === "error"
+          ? "bg-red-700"
+          : "bg-lime-500 text-black"
+      )}
+      transitionClassName="[&]:-translate-y-20 [&]:opacity-100"
+      onTransitionEnd={handleTransitionEnd}
+    >
+      {message.current?.text}
+    </Transition>
+  );
 }
 
 export default observer(Snackbar);
