99f231ed4c6ab465d12565bfada1cfd3fe73f570
Javier Robles <javiroblesfernandez@gmail.com>
2022-11-04 10:57:48 +0100

    Remove useLayouteffect

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,11 +1,9 @@
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export function useEvent<T>(handler: (...args: T[]) => unknown) {
   const ref = useRef(handler);
 
-  useLayoutEffect(() => {
-    ref.current = handler;
-  });
+  ref.current = handler;
 
   return useCallback((...args: T[]) => {
     const fn = ref.current;
