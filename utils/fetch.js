8c27c1c78a4d6158a4637c48b3ecd45e22b089e7
Javier Robles <javiroblesfernandez@gmail.com>
2023-06-14 18:13:34 +0200

    Add fetch

---
+++ utils/fetch.js
@@ -0,0 +1,66 @@
+/** @param {Response} response */
+const parseResponse = async (response) => {
+	const text = await response.text();
+
+	try {
+		return JSON.parse(text);
+	} catch {
+		return text;
+	}
+};
+
+/**
+ * @param {string} url
+ * @param {RequestInit} [init]
+ */
+export const request = async (url, init) => {
+	const response = await fetch(url, init);
+	return await parseResponse(response);
+};
+
+/** @param {string} url */
+export const get = (url) => {
+	return request(url, {
+		credentials: "include",
+	});
+};
+
+/**
+ * @param {string} url
+ * @param {object} body
+ */
+export const post = (url, body) => {
+	return request(url, {
+		credentials: "include",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+/**
+ * @param {string} url
+ * @param {Record<string, string | Blob | FileList>} data
+ */
+export const upload = (url, data) => {
+	const formData = new FormData();
+
+	Object.entries(data).forEach(([key, value]) => {
+		if (value instanceof FileList) {
+			for (let i = 0; i < value.length; i += 1) {
+				formData.append(key, value[i]);
+			}
+
+			return;
+		}
+		formData.append(key, value);
+	});
+
+	return request(url, {
+		credentials: "include",
+		method: "POST",
+		body: formData,
+	});
+};
