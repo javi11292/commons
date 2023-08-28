d88e086df423bd184ab4e7b708ddde65cac8a24b
Javier Robles <javiroblesfernandez@gmail.com>
2023-08-28 17:35:26 +0200

    Improve button padding

--- components/button/button.svelte
+++ components/button/button.svelte
@@ -67,12 +67,12 @@
 		overflow: hidden;
 		outline: none;
 		cursor: pointer;
-		padding: 1rem 0.5rem;
+		padding: 1rem;
 		font-weight: bold;
 	}
 
 	.sm {
-		padding: 0.5rem;
+		padding: 0.5rem 1rem;
 		font-size: 0.9rem;
 	}
 
