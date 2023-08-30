9939c254b41d17f8398980b7c8eb9c69c92ecbe6
Javier Robles <javiroblesfernandez@gmail.com>
2023-08-30 09:48:31 +0200

    Add target

--- utils/portal.ts
+++ utils/portal.ts
@@ -1,5 +1,7 @@
-export const portal = (node: HTMLElement) => {
-	document.body.appendChild(node);
+export const portal = (node: HTMLElement, target?: Element | null) => {
+	const targetElement = target || document.body;
+
+	targetElement.appendChild(node);
 	node.hidden = false;
 
 	return { destroy: () => node.parentNode && node.parentNode.removeChild(node) };
