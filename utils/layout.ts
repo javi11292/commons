10b3769816c8624c336182c7d1837e62a34679cf
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-06 17:25:18 +0100

    Astro remove layout file

--- utils/layout.ts
+++
@@ -1,6 +0,0 @@
-import { browser, dev } from "$app/environment";
-import "$lib/commons/assets/global.scss";
-
-if (browser && !dev && navigator.serviceWorker) {
-	navigator.serviceWorker.register("/service-worker.js");
-}
