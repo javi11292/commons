5b766903298fa5be02a106c725072cdab3131053
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-10 17:32:12 +0100

    Test preload

--- components/icon/icon.svelte
+++ components/icon/icon.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	export let icon: string;
+
+	$: image = `/icons/${icon}.svg#svg`;
 </script>
 
+<svelte:head>
+	<link rel="preload" as="image" href={image} />
+</svelte:head>
+
 <svg viewBox="0 0 24 24" fill="currentColor" class="icon">
-	<use href={`/icons/${icon}.svg#svg`} />
+	<use href={image} />
 </svg>
 
 <style lang="scss">
