a1eadc7a314dffda3ca2e737991ebc1de08acb01
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-12 19:55:58 +0100

    Add fade component

---
+++ components/fade/fade.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
+
+	export let disabled = false;
+
+	let render = false;
+
+	onMount(() => (render = true));
+</script>
+
+{#if disabled}
+	<slot />
+{:else if render === true}
+	<div in:fade on:introend={() => (disabled = true)}>
+		<slot />
+	</div>
+{/if}
