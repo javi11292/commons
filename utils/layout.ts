7bf15436bf0eff5b590afb516bae9ca9a6cb27f1
Javier Robles <javiroblesfernandez@gmail.com>
2024-04-06 19:46:25 +0200

    Refactor

--- utils/layout.ts
+++ utils/layout.ts
@@ -2,7 +2,7 @@ import { browser, dev } from "$app/environment";
 import { onNavigate } from "$app/navigation";
 import "$lib/commons/assets/global.scss";
 
-export const layout = () => {
+export const setup = () => {
 	if (browser && !dev && navigator.serviceWorker) {
 		navigator.serviceWorker.register("/service-worker.js");
 	}
