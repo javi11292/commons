a387233076ec919b66062671d959d1ddc85b40e7
Javier Robles <javiroblesfernandez@gmail.com>
2024-04-07 14:49:31 +0200

    Refactor dialog

--- components/modal/modal.svelte
+++ components/modal/modal.svelte
@@ -1,53 +1,131 @@
 <script lang="ts">
-	import { portal } from "$lib/commons/utils/portal";
 	import type { Snippet } from "svelte";
-	import { fade, scale } from "svelte/transition";
+	import type { AnimationEventHandler } from "svelte/elements";
 
-	let { open, children }: { open?: boolean; children: Snippet } = $props();
+	// eslint-disable-next-line no-undef
+	let { open = $bindable(false), children }: { open?: boolean; children: Snippet } = $props();
+
+	const handleClick = () => {
+		open = false;
+	};
+
+	let dialog = $state<HTMLDialogElement>();
+	let keepOpen = $state(false);
+
+	const handleAnimation =
+		(start: boolean): AnimationEventHandler<HTMLDialogElement> =>
+		(event) => {
+			if (!event.pseudoElement) {
+				keepOpen = open || start;
+			}
+		};
+
+	$effect(() => {
+		dialog?.showModal();
+	});
 </script>
 
-{#if open}
-	<div
-		on:click={() => (open = false)}
-		on:keypress
-		use:portal
+{#if open || keepOpen}
+	<dialog
+		bind:this={dialog}
+		onclick={handleClick}
+		class:reverse={!open && keepOpen}
+		role="presentation"
+		onanimationstart={handleAnimation(true)}
+		onanimationend={handleAnimation(false)}
+	>
+		{@render children()}
+	</dialog>
+{/if}
+
+<!-- <div
+		onclick={handleClick}
+		onkeypress={handleClick}
 		hidden
 		tabindex="0"
 		role="button"
 		class="backdrop"
-		transition:fade
+		transition:fade={{ duration: 200 }}
 	>
-		<div transition:scale class="modal">
+		<div transition:scale={{ duration: 200 }} class="modal">
 			<div class="content">
 				{@render children()}
 			</div>
 		</div>
 	</div>
-{/if}
+  -->
 
 <style lang="scss">
-	.backdrop {
-		position: fixed;
-		inset: 0;
-		z-index: 10;
-		display: flex;
-		align-items: center;
-		justify-content: center;
+	::backdrop {
 		background: rgba(0, 0, 0, 0.8);
 	}
 
-	.modal {
-		display: flex;
-		max-height: 90vh;
-		max-width: 90vw;
-		overflow: hidden;
-		border-radius: 0.25rem;
-		background: var(--background);
-		box-shadow: 0 4px 6px black;
+	.reverse {
+		animation: disappear 200ms;
 	}
 
-	.content {
-		overflow-y: auto;
-		padding: 1rem;
+	.reverse::backdrop {
+		animation: fadeOut 200ms;
+	}
+
+	dialog {
+		animation: appear 200ms;
+		border: none;
+		border-radius: 0.25rem;
+		box-shadow: 0 4px 6px black;
+		max-width: 90dvw;
+		max-height: 90dvh;
+	}
+
+	dialog::backdrop {
+		animation: fadeIn 200ms;
+	}
+
+	@keyframes fadeIn {
+		from {
+			opacity: 0;
+		}
+
+		to {
+			opacity: 1;
+		}
+	}
+
+	@keyframes fadeOut {
+		from {
+			opacity: 1;
+		}
+
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes appear {
+		from {
+			opacity: 0;
+			scale: 0;
+			display: none;
+		}
+
+		to {
+			opacity: 1;
+			scale: 1;
+			display: block;
+		}
+	}
+
+	@keyframes disappear {
+		from {
+			opacity: 1;
+			scale: 1;
+			display: block;
+		}
+
+		to {
+			opacity: 0;
+			scale: 0;
+			display: none;
+		}
 	}
 </style>
